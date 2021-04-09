cb3feb7d7b289c0935d705957222a066fcdf3efd
unknown <you@example.com>
2021-04-09 09:33:09 -0500

    used state and hooks, created and used tests

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render,screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event';
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>);
+    const header = screen.queryByText(/form/i)
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm/>);
+    const firstName =screen.getByLabelText(/first name/i);
+    const lastName =screen.getByLabelText(/last name/i);
+    const address =screen.getByLabelText(/address/i);
+    const city =screen.getByLabelText(/city/i);
+    const state =screen.getByLabelText(/state/i);
+    const zip =screen.getByLabelText(/zip/i);
+    const button = screen.getByRole("button");
+
+    userEvent.type(firstName, 'Luis');
+    userEvent.type(lastName, 'Garcia');
+    userEvent.type(address, '2132 E WISCONSIN RD');
+    userEvent.type(city, 'Edinburg');
+    userEvent.type(state, 'TX');
+    userEvent.type(zip, '78542');
+    userEvent.click(button);
+
+    const success =screen.queryByText(/woo-hoo/i);
+    expect(success).toBeInTheDocument()
+
+
+
+});
